c4ad1691a086038ad5c5e48da291103f93771c7e
Jingyan-Chen <79214648+Jingyan-Chen@users.noreply.github.com>
2021-02-21 09:33:51 +0800

    Add files via upload

---
+++ database_table_create.sql
@@ -0,0 +1,39 @@
+
+--create database VirtualTeaClub;
+
+use VirtualTeaClub;
+
+create table Userr
+(name char(30) not null,
+UserID int primary key
+);
+
+create table Tea
+(name char(30) not null,
+TeaID int primary key,
+score float
+);
+
+create table UT
+(UserID int not null,
+TeaID int not null,
+number int not null,
+foreign key (UserID) references Userr(UserID),
+foreign key (TeaID) references Tea(TeaID)
+);
+
+create table Comments
+(TeaID int not null,
+comment char(300) not null,
+score float not null
+);
+
+--drop table UT;
+--drop table Tea;
+--drop table Userr;
+--drop table Comments;
+
+--select* from Userr;
+--select* from Tea;
+--select* from UT;
+--select* from Comments;
